8d7d8773ecaa032949407a331ce81fc2d178aee4
Oscar Chan <oscarchankalung@gmail.com>
2022-04-26 20:56:56 +0800

    add 23-next-js

---
+++ 23-next-js/pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://oscarchankalung:byo8A220Al1sTtnS@cluster0.vccxw.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
---
+++ 23-next-js/pages/new-meetup/index.js
@@ -0,0 +1,39 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { Fragment } from "react";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewPage() {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.replace("/");
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups to create networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </Fragment>
+  );
+}
+
+export default NewPage;
